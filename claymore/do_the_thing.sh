c8628237db0fa75ad10a4898efdd116065eaecb9
Duncan <theboss723@gmail.com>
2017-06-06 21:32:52 -0700

    added claymore

---
+++ claymore/do_the_thing.sh
@@ -0,0 +1,2 @@
+./ethdcrminer64 -epool exp-eu.dwarfpool.com:8018 -epool exp-us.dwarfpool.com:8018 -ewal 0xd5a7eda7b3cd20ef383e501373737f0880f41ee9 -epsw x -ethi 16 -mode 1 -dbg -1 -mport 0 -allpools 1 -allcoins exp
+
